2ac0f67b196265091067db5549963e630180076d
Jit nath <150388284+Jeet-web2026@users.noreply.github.com>
2025-01-06 16:16:35 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
 $(document).ready(function () {
     $('#main .navigation-btns a').on('click', function () {
         $(this).addClass('bg-black text-light');
         $(this).siblings().removeClass('bg-black text-light');
     });
-=======
-$(document).ready(function(){
-
->>>>>>> 5076020b5c6af8ad531f327688a717b4219cdf39
-});
