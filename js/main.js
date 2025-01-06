14e9a681414121401c1661a3f2c468451891db20
DIGITYX <digismartskills12@gmail.com>
2025-01-06 17:35:28 +0530

    j

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    $('#main .navigation-btns a').eq(0).addClass('bg-black text-light');
     $('#main .navigation-btns a').on('click', function () {
         $(this).addClass('bg-black text-light');
         $(this).siblings().removeClass('bg-black text-light');
